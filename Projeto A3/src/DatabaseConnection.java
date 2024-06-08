52b5bb90eed7cc1d3c9801c01ab7621824c629f8
ERICK DÍAZ <ERICK DAZ@192.168.1.179>
2024-06-08 13:23:35 -0300

    Primeiro commit

---
+++ Projeto A3/src/DatabaseConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/energy_store";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the database.");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
